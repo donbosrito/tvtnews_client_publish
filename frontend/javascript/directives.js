c973654982239b85e3e83784b6d8e9ec8b9324fe
Don <lebangtu95@gmail.com>
2017-01-10 00:01:27 +0700

    no message

--- frontend/javascript/directives.js
+++ frontend/javascript/directives.js
@@ -955,23 +955,24 @@
             controller: ['$http', '$routeParams', '$window', '$location', function ($http, $routeParams, $window, $location) {
                 var controller = this;
 
-                window.fbAsyncInit = function() {
-                    FB.init({
-                        appId      : '402463633430661',
-                        xfbml      : true,
-                        version    : 'v2.8'
-                    });
-                };
 
-                (function(d, s, id){
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) {return;}
-                    js = d.createElement(s); js.id = id;
-                    js.src = "//connect.facebook.net/en_US/sdk.js";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
 
                 controller.fbShare = function () {
+                    window.fbAsyncInit = function() {
+                        FB.init({
+                            appId      : '155342231619633',
+                            xfbml      : true,
+                            version    : 'v2.8'
+                        });
+                    };
+
+                    (function(d, s, id){
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) {return;}
+                        js = d.createElement(s); js.id = id;
+                        js.src = "//connect.facebook.net/en_US/sdk.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
                     FB.ui(
                         {
                             method: 'feed',
